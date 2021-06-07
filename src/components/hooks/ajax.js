5de9006a001b491ecb9bae5025bf05c8c7330454
FIZZO PANNOSCH <fizzo999@gmail.com>
2021-06-06 19:50:02 -0700

    ajax hooks now working correctly

--- src/components/hooks/ajax.js
+++ src/components/hooks/ajax.js
@@ -2,21 +2,22 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const useAjax = () => {
-
+  
   const [options, setOptions] = useState({});
   const [response, setResponse] = useState({});
   const [error, setError] = useState({});
   const [isLoading, setIsLoading] = useState({});
-
+  console.log('WE ARE IN AJAX HOOK', response);
+  
   useEffect(() => {
     async function ajax() {
       if (!options) {
         console.log('You have NOT specified any request options');
-        return;}
+        return};
       setIsLoading(true);
       try {
         const res = await axios(options);
-        setResponse(res.data);
+        await setResponse(res.data);
         setIsLoading(false);
       } catch(error) {
         setError(error);
